94fd3e2c7a4c1dac940ef29f1d051b1929604a66
josiahdc <josiah.chapman@gmail.com>
2021-03-08 23:05:27 -0800

    undid nullptr change to receive on network socket

--- src/NetworkSocket.cpp
+++ src/NetworkSocket.cpp
@@ -15,16 +15,13 @@ void NetworkSocket::setSubscriptionFilter(const string &filter) {
 
 unique_ptr<multipart_t> NetworkSocket::receive(recv_flags flags) {
     auto message = make_unique<multipart_t>();
-    if (message->recv(socket, static_cast<int>(flags))) {
-        return message;
-    } else {
-        return nullptr;
-    }
+    message->recv(socket, static_cast<int>(flags));
+    return message;
 }
 
 unique_ptr<char[]> NetworkSocket::receiveBuffer(recv_flags flags) {
     auto envelope = receive(flags);
-    if (envelope != nullptr) {
+    if (!envelope->empty()) {
         auto message = envelope->pop();
         return alignBuffer(message.data(), message.size());
     } else {
