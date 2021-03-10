6785ec4ff56565606a0d69d8ded2e60dcde76342
josiahdc <josiah.chapman@gmail.com>
2021-03-09 17:47:51 -0800

    split Time into header and source file

--- include/Time.h
+++ include/Time.h
@@ -5,14 +5,9 @@
 
 namespace impresarioUtils {
 
-uint64_t getCurrentTime() {
-    auto rawTime = std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now());
-    return rawTime.time_since_epoch().count();
-}
+uint64_t getCurrentTime();
 
-uint64_t getElapsedTime(uint64_t initialTime) {
-    return getCurrentTime() - initialTime;
-}
+uint64_t getElapsedTime(uint64_t initialTime);
 
 }
 
