c786df7d89b02157725d6a22698b16aeea178d2a
josiahdc <josiah.chapman@gmail.com>
2021-03-30 12:52:10 -0700

    refactored lib to single header

---
+++ include/ImpresarioUtils.h
@@ -0,0 +1,11 @@
+#ifndef IMPRESARIO_UTILS_IMPRESARIOUTILS_H
+#define IMPRESARIO_UTILS_IMPRESARIOUTILS_H
+
+#include "NetworkProxy.h"
+#include "NetworkSocket.h"
+#include "NonCopyable.h"
+#include "RandomNumberGenerator.h"
+#include "SerializedData.h"
+#include "Time.h"
+
+#endif //IMPRESARIO_UTILS_IMPRESARIOUTILS_H
