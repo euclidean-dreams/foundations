fd6bf1e99e3b96a86253b1fbc14d4650c687df10
josiahdc <josiah.chapman@gmail.com>
2021-03-30 15:32:03 -0700

    fixed single header

--- include/ImpresarioUtils.h
+++ include/ImpresarioUtils.h
@@ -1,11 +1,11 @@
 #ifndef IMPRESARIO_UTILS_IMPRESARIOUTILS_H
 #define IMPRESARIO_UTILS_IMPRESARIOUTILS_H
 
-#include "NetworkProxy.h"
-#include "NetworkSocket.h"
-#include "NonCopyable.h"
-#include "RandomNumberGenerator.h"
-#include "SerializedData.h"
-#include "Time.h"
+#include "../src/NetworkProxy.h"
+#include "../src/NetworkSocket.h"
+#include "../src/NonCopyable.h"
+#include "../src/RandomNumberGenerator.h"
+#include "../src/SerializedData.h"
+#include "../src/Time.h"
 
 #endif //IMPRESARIO_UTILS_IMPRESARIOUTILS_H
