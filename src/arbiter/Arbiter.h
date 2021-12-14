2b7d3f9923a1bd2cab21e6d18223a246798a1880
josiahdc <josiah.chapman@gmail.com>
2021-12-13 17:42:47 -0800

    added new data toggle

--- src/arbiter/Arbiter.h
+++ src/arbiter/Arbiter.h
@@ -12,22 +12,30 @@ class Arbiter : public NonCopyable {
 private:
     std::mutex mutex;
     std::shared_ptr<const T> arbitable;
+    bool newData;
 
 public:
     Arbiter()
             : mutex{},
-              arbitable{nullptr} {
+              arbitable{nullptr},
+              newData{false} {
 
     }
 
     void give(std::unique_ptr<const T> newArbitable) {
         std::unique_lock<std::mutex> lock{mutex};
         arbitable = move(newArbitable);
+        newData = true;
     }
 
     std::shared_ptr<const T> take() {
         std::unique_lock<std::mutex> lock{mutex};
+        newData = false;
         return arbitable;
+    }
+
+    bool newDataAvailable() const {
+        return newData;
     }
 };
 
